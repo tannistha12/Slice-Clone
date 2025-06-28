79cb7ccd9581490f08d82053ff04d2e82d2bb8c7
tannistha12 <tannisthachat17@gmail.com>
2025-06-28 12:37:42 +0530

    tailwind setup done

--- client/client/src/App.jsx
+++ client/client/src/App.jsx
@@ -28,6 +28,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div className="bg-red-500 text-white p-4 text-xl">Tailwind is working!</div>
+
     </>
   )
 }
